e4f6a7c637f29cea04abb0b07c058320574845f1
Vasileios Peitos <vpeitos@gmail.com>
2017-06-27 22:28:01 +0200

    Full Functional Tic Tac Toe
    function testEnd, tests if the board is full
    function restart, redraws the board
    function showNewGameButton, When the game is paused show the New Game
    Button to start a new game
    function translateCoordToCell, translates the middle x,y coordinates to
    the cell number of the array

--- sketch.js
+++ sketch.js
@@ -3,47 +3,38 @@ colorMode(RGB, 255, 255, 255, 100);
 var roundNr;
 
 
+
 /*<--- SETUP --->*/
 function setup() {
 
-    createCanvas(467, 487);
-    //background(248);
-    game = new Game();
-    game.fillArray();
-    console.log(game.cells[0]);
-    initialState();
+    createCanvas(467, 487); //width 467px, height 487px
 
-    roundNr = 0;
+    game = new Game();  //Create new Game Object
+    game.fillArray();   //initializes the board Cells with 0's
+
+    initialState();     //draws a blank Board
+
+    roundNr = 0;        //counter keeps the number of rounds mostly to decide whos turn it is
 }
 
 /*<--- DRAW --->*/
 function draw() {
 
-    console.log(game.pause);
-    if(!game.pause){
-        whosTurn();
-    }
-    game.testWin();
-
-
-    if (roundNr == 11) {
-
-        initialState();
-
-        roundNr = 0;
+    if(game.play){
+        whosTurn();     //Shows whos turn it is
     }
 
+    game.testWin();     //has anybody won already?
+    game.testEnd();     //is the Board filled?
 
 }
 
-function Game() {
+function Game() {       //the Game Object
 
-        this.cells = [];
-        this.pause = false;
+        this.cells = [];    //array that keeps track of which Cell is filled with Xs or Os
+        this.play = true;   //boolean that shows play or pause state
 
-
-
-        this.testWin = function() {
+        this.testWin = function() { //the function tests all possible win cases for X or O
             var winner = -1;
 
             for(var i = 0; i < 3; i++){
@@ -60,13 +51,13 @@ function Game() {
 
                             winner = game.cells[i * 3];
                 }
-                else if(game.cells[0] != 0
+                else if(game.cells[0] != 0      //Diagonal Check 1
                         && game.cells[0] == game.cells[4]
                         && game.cells[4] == game.cells[8]) {
 
                             winner = game.cells[4];
                 }
-                else if(game.cells[2] != 0
+                else if(game.cells[2] != 0      //Diagonal Check 2
                         && game.cells[2] == game.cells[4]
                         && game.cells[4] == game.cells[6]) {
 
@@ -74,18 +65,36 @@ function Game() {
                 }
 
             }
-            if(winner != -1){
-                wonState(winner);
+            if(winner != -1){           //if there is a winner
+                wonState(winner);       //draw the winner Screen
+                this.play = false;
+                showNewGameButton();    //show New Game Button to start a new Game
             }
         }
-        this.testEnd = function() {
-            var isThisTheEnd = false;
+
+        this.testEnd = function() {     //Checks if the Board is full
+            var isThisTheEnd = true;
+
+            for(var i = 0; i < 9; i++){
+                if(game.cells[i] == 0){ //if at least one cell is empty go on
+                    isThisTheEnd = false;
+                }
+            }
+
+            if(isThisTheEnd){
+                this.play = false;
+                showNewGameButton();    //show New Game Button to start a new Game
+            }
 
         }
-        this.restart = function() {
 
+        this.restart = function() {     //Restart the Game
+            this.play = true;
+            game.fillArray();           //empty the Cell array (fill it with 0s)
+            initialState();             //draw the empty board
         }
-        this.fillArray = function() {
+
+        this.fillArray = function() {   ////The function that empties the Cell array (filling it with 0s)
             for(var i=0; i<9; i++){
                 this.cells[i] = 0;
             }
@@ -93,7 +102,7 @@ function Game() {
 }
 
 
-function initialState() {
+function initialState() {           //Draw an empty Board
 
     background(248);
 
@@ -102,6 +111,7 @@ function initialState() {
     textSize(32);
     textAlign(CENTER);
     text("TICK TACK", 233, 45);
+
     /*<--- Tablo --->*/
     noStroke();
     fill(138, 133, 189);
@@ -122,29 +132,28 @@ function initialState() {
     rect(81, 292, 306, 9);
 }
 
-function wonState(winner) { //add later: parameter for circle and cross
+function wonState(winner) {
 
-    game.pause = true;
     //Grey background
     noStroke();
     fill(236, 236, 238);
     rect(0, 68, 467, 354);
 
-     if (winner == 2) {
+     if (winner == 2) { // 2 for Winner is the player with O's
         //redundant
         noFill();
         strokeWeight(15);
         /*<--- Circle --->*/
         stroke(115, 220, 230);
-        ellipse(234, 225, 134, 134);
+        ellipse(234, 225, 134, 134);    //Big Circle
 
     }
-    else if(winner == 1) {
+    else if(winner == 1) { // 1 for Winner is the player with X's
 
         noStroke();
         fill(58, 59, 99);
 
-        crossX(234, 225, 134, 15);
+        crossX(234, 225, 134, 15);      //Big Cross
     }
 
     //text: Won the game
@@ -158,24 +167,28 @@ function wonState(winner) { //add later: parameter for circle and cross
 
 function mousePressed() {
 
-
-    if (roundNr % 2 == 0) {
-        drawCrossX();
+    if(game.play){                  //While we are still playing
+        if (roundNr % 2 == 0) {     //for even round numbers draw a Cross in the Cell
+            drawCrossX();
+        }
+        else {
+            drawCircle();           //for odd round numbers draw a Circle in the Cell
+        }
     }
-    else {
-        drawCircle();
+    else {                          //when the game is paused cause of full Board or Winner (New game button is showing)
+        if((mouseX > 155 && mouseX < 312) && (mouseY > 433 && mouseY < 475)){      // Click on New Game Button
+            game.restart();                                                        // and start a new Game
+        }
     }
-
-    console.log("Round Nr = " + roundNr);
 }
 
 function whosTurn() {
-    //Hide Turn
+    //Hide Message Whos Turn it is
     noStroke();
     fill(248);
     rect(171,434,150,41);
 
-    if(game.play) {
+    if(game.play) {             //While we are playing Show whos turn it is
         if(roundNr%2 == 0) {
             noStroke();
             fill(58, 59, 99);
@@ -188,6 +201,7 @@ function whosTurn() {
             ellipse(192, 454, 35, 35);
         }
 
+        //text = "s TURN"
         fill(184, 184, 190);
         noStroke();
         textSize(15);
@@ -196,7 +210,104 @@ function whosTurn() {
     }
 }
 
-function translateCoordToCell(CoordX, CoordY) {
+function showNewGameButton() {  //When the game is paused show the New Game Button to start a new game
+    fill(33, 195, 168);
+    noStroke();
+    rect(155, 433, 157, 43);
+    ellipse(159,455,42,43);
+    ellipse(312,455,42,43);
+
+    fill(255);
+    textSize(15);
+    textAlign(CENTER);
+    text("NEW GAME", 234, 454);
+}
+
+function drawCircle() {     //draw a circle
+    var index = -1;
+
+    noFill();
+    strokeWeight(9);
+    /*<--- Circle --->*/
+    stroke(115, 220, 230);
+
+    var x = drawOnGridX();  //draw it in the Cell boundaries
+    var y = drawOnGridY();  // the x, y values contain the Coordinates of the center of the cell
+
+    if(x != -1 && y != -1){
+        index = translateCoordToCell(x,y);  //returns in which cell the player clicked
+        if(game.cells[index] == 0){
+            game.cells[index] = 2; // 1 for Cross, 0 for Circle
+            roundNr++;
+            ellipse(x, y, 63, 63);
+        }
+
+    }
+
+}
+
+function drawCrossX() {
+    noStroke();
+    fill(58, 59, 99);
+
+    var x = drawOnGridX();
+    var y = drawOnGridY();
+
+    if(x != -1 && y != -1) {
+        index = translateCoordToCell(x,y);
+        if(game.cells[index] == 0){
+            game.cells[index] = 1; // 1 for Cross, 0 for Circle
+            roundNr++;
+            crossX(x, y, 73, 9);
+        }
+    }
+}
+
+/*<--- Custom Shape Function Cross --->*/
+function crossX(x, y, w, h) {
+    rectMode(CENTER);
+
+    rect(x, y, w, h);
+    rect(x, y, h, w);
+    rectMode(CORNER);
+}
+
+// Cross and Circles are placed on the Grid
+function drawOnGridX() { //returns the center (middle) x Coordinate of each cell
+
+    var x = -1;
+
+    if(mouseX > 81 && mouseX < 177) {
+        x = 129;
+    }
+    else if(mouseX > 186 && mouseX < 282) {
+        x = 234;
+    }
+    else if(mouseX > 291 && mouseX < 387) {
+        x = 339;
+    }
+
+    return x;
+}
+
+function drawOnGridY() {    //returns the center (middle) y Coordinate of each cell
+
+    var y = -1;
+
+    if(mouseY > 91 && mouseY < 187) {
+        y = 139;
+    }
+    else if(mouseY > 196 && mouseY < 292) {
+        y = 244;
+    }
+    else if(mouseY > 301 && mouseY < 397) {
+        y = 349;
+    }
+
+    return y;
+}
+
+function translateCoordToCell(CoordX, CoordY) { //translates the middle coordiantes to the cell number of the array
     if(CoordX == 129){
         x = 0;
     }
@@ -216,94 +327,6 @@ function translateCoordToCell(CoordX, CoordY) {
     else if(CoordY == 349){
         y = 2;
     }
-    console.log(x+(y*3));
+
     return x+(y*3);
-}
-
-function drawCircle() {
-    var index = -1;
-
-    noFill();
-    strokeWeight(9);
-    /*<--- Circle --->*/
-    stroke(115, 220, 230);
-
-    var x = drawOnGridX();
-    var y = drawOnGridY();
-
-    console.log("drawCircle(): x=" + x + " y = " + y);
-
-    if(x != -1 && y != -1){
-        index = translateCoordToCell(x,y);
-        if(game.cells[index] == 0){
-            game.cells[index] = 2;
-            roundNr++;
-            ellipse(x, y, 63, 63);
-        }
-
-    }
-
-}
-
-function drawCrossX() {
-    noStroke();
-    fill(58, 59, 99);
-
-    var x = drawOnGridX();
-    var y = drawOnGridY();
-
-    if(x != -1 && y != -1) {
-        index = translateCoordToCell(x,y);
-        if(game.cells[index] == 0){
-            game.cells[index] = 1;
-            roundNr++;
-            crossX(x, y, 73, 9);
-        }
-    }
-}
-
-/*<--- Custom Shape Function Cross --->*/
-function crossX(x, y, w, h) {
-    rectMode(CENTER);
-
-    rect(x, y, w, h);
-    rect(x, y, h, w);
-    rectMode(CORNER);
-}
-
-// Cross and Circles are placed on the Grid
-function drawOnGridX() {
-
-    var x = -1;
-
-    if(mouseX > 81 && mouseX < 177) {
-        x = 129;
-    }
-    else if(mouseX > 186 && mouseX < 282) {
-        x = 234;
-    }
-    else if(mouseX > 291 && mouseX < 387) {
-        x = 339;
-    }
-
-    return x;
-
-}
-
-function drawOnGridY() {
-
-    var y = -1;
-
-    if(mouseY > 91 && mouseY < 187) {
-        y = 139;
-    }
-    else if(mouseY > 196 && mouseY < 292) {
-        y = 244;
-    }
-    else if(mouseY > 301 && mouseY < 397) {
-        y = 349;
-    }
-
-    return y;
-
 }
